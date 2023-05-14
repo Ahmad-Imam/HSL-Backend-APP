75b403c4c78ae53edf9e050e4a43b1927fccc7fc
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2023-05-14 23:13:56 +0300

    testing pagination

--- data.controller.js
+++ data.controller.js
@@ -19,12 +19,39 @@ class GroupController {
       })
       .on("end", () => {
         console.log(journeyList.length);
+        console.log(journeyList[0]);
+
         response.sendStatus(200);
         console.timeEnd(__filename);
       });
   }
 
-  JourneyListByPage(request, response, next) {}
+  JourneyListByPage(request, response, next) {
+    console.log("in");
+    const PAGE_SIZE = 100;
+    try {
+      const pageNumber = parseInt(request.body.pageNumber) || 1;
+      const startIndex = (pageNumber - 1) * PAGE_SIZE;
+      const endIndex = pageNumber * PAGE_SIZE;
+
+      const paginatedJourneyList = journeyList.slice(startIndex, endIndex);
+
+      var finalJourneyList = [];
+      paginatedJourneyList.forEach((item, index) => {
+        finalJourneyList.push(item);
+      });
+
+      response.json({
+        journeyList: paginatedJourneyList.length,
+        finaljourneyListLength: finalJourneyList.length,
+        currentPage: pageNumber,
+        totalPages: Math.ceil(journeyList.length / PAGE_SIZE),
+        finaljourneyList: finalJourneyList,
+      });
+    } catch (err) {
+      response.status(500).json({ message: err.message });
+    }
+  }
 
   StationList(request, response, next) {}
 

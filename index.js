9102644f49a966b807b86c6c771e5b5cbe8381b7
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2023-05-21 00:01:18 +0300

    added documentation jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+/**
+ * Module dependencies.
+ */
 const express = require("express");
 
+/**
+ * Express app configuration.
+ */
 var app = express();
 app.use(express.json());
 app.use(
@@ -8,17 +14,28 @@ app.use(
   })
 );
 
+/**
+ * Routes
+ */
 var indexRouter = require("./routes");
 
 app.use("/", indexRouter);
-const hostname = "0.0.0.0";
-var port = 8080;
 
+/**
+ * Error handling middleware
+ */
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
+/**
+ * Start the server
+ */
+const hostname = "0.0.0.0";
+var port = 8080;
+
 app.listen(process.env.PORT || port, hostname, () =>
   console.log(`App is running on port ${port}`)
 );
+
 module.exports = app;
